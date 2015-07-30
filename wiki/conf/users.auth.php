694cb4a6ca40d25608f8a9588cc9ea84632d64d8
Maik Wagner <wagner@mcwiwa.de>
2015-07-30 11:37:31 +0200

    barcampruhr8 verschoben, Kirby installiert

---
+++ wiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+barcamp:$1$OicYCONs$mgUccXh5J4/A0rfWuuqNL0:Barcamp Ruhr:mail@barcamp.ruhr:admin,user
