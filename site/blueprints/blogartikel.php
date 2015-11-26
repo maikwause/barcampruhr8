3c1ff902e4386a06834fa2b125e3ad95ea8e4234
unknown <E:\AllesDaten\Bat-Daten>
2015-11-26 16:58:08 +0100

    Bearbeitungsmaske für Blogartikel ergänzt, damit das Datum nicht flöten geht

---
+++ site/blueprints/blogartikel.php
@@ -0,0 +1,15 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Page
+pages: true
+files: true
+fields:
+  title:
+    label: Title
+    type:  text
+  date:
+  	label: Datum
+  	type: text
+  text:
+    label: Text
+    type:  textarea
