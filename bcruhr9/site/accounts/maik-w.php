37f95a41678cc326bb9f7a8187ce53ffcd8d1734
unknown <E:\AllesDaten\Bat-Daten>
2016-01-22 11:48:05 +0100

    Netzkommune und IOM als Sponsoren hinzugefügt

--- bcruhr9/site/accounts/maik-w.php
+++ bcruhr9/site/accounts/maik-w.php
@@ -7,8 +7,8 @@ password: >
 language: de
 role: admin
 history:
+  - sponsoren/iom
+  - sponsoren/netzkommune
   - kontakt/impressum
   - blog/tickets-sponsoren-weihnachten
   - teaser/sponsoren
-  - tickets
-  - teaser/das-barcamp
