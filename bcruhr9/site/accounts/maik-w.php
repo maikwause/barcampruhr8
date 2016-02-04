b917d223726820c10bb8ec970c62149309e9b27f
unknown <E:\AllesDaten\Bat-Daten>
2016-02-04 15:17:37 +0100

    Lokalkompass jetzt auch verlinkt

--- bcruhr9/site/accounts/maik-w.php
+++ bcruhr9/site/accounts/maik-w.php
@@ -7,8 +7,8 @@ password: >
 language: de
 role: admin
 history:
+  - sponsoren/lokalkompass
   - blog/weitere-tickets
   - tickets/teilnehmer
   - tickets
   - sponsoren/iom
-  - sponsoren/netzkommune
