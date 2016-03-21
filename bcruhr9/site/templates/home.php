b45bff07a583ade70f8a45a2ca8135384699f19f
unknown <E:\AllesDaten\Bat-Daten>
2016-03-21 10:20:33 +0100

    Sessionboards von der Startseite runter

--- bcruhr9/site/templates/home.php
+++ bcruhr9/site/templates/home.php
@@ -1,15 +1,4 @@
 <?php snippet('header') ?>
-<!--
-<section class="know-about-us">
-	<div class="center clearfix">
-		<img src="<?php echo url('assets/images/idea-thought.png')?>" alt="">
-		<div class="text">
-			<?php echo $page->text()->kirbytext() ?>
-		</div>
-	</div>
-</section>
--->
-<?php snippet('sessions') ?>
 <?php snippet('teasers') ?>
 <?php snippet('sponsors') ?>
 <?php snippet('footer') ?>
