e2698c26ec5a47f073a23ebde342545805ced9c4
PLACKPEAUTYII\Maik <wagner@mcwiwa.de>
2016-11-21 16:49:51 +0100

    Teaser wieder auf Anfang umgestellt

--- bcruhrX/site/accounts/maik-w.php
+++ bcruhrX/site/accounts/maik-w.php
@@ -7,8 +7,8 @@ password: >
 language: de
 role: admin
 history:
-  - sponsoren/agile-ruhr
-  - archiv
-  - archiv/bcruhr8
-  - blog
+  - blog/save-the-date-bcruhrx
+  - kontakt/organisatoren
   - kontakt
+  - infos/location
+  - infos
