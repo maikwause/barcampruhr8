9f6fbd5989c59435d6f75be6749d083dd26affde
PLACKPEAUTYII\Maik <wagner@mcwiwa.de>
2016-11-29 20:42:33 +0100

    Link zur Teilnehmerliste

--- bcruhrX/site/accounts/maik-w.php
+++ bcruhrX/site/accounts/maik-w.php
@@ -7,8 +7,8 @@ password: >
 language: de
 role: admin
 history:
+  - tickets/teilnehmer
   - blog/tickets-tickets-tickets
   - tickets
   - kontakt/organisatoren
   - archiv/bcruhr9
-  - archiv/bcruhr8
