1e76ee00ffa5b6ad80246f888a352ede7d42400b
PLACKPEAUTYII\Maik <wagner@mcwiwa.de>
2017-03-20 13:48:02 +0100

    keine Tickets mehr -> Tauschbörse

--- bcruhrX/site/accounts/maik-w.php
+++ bcruhrX/site/accounts/maik-w.php
@@ -7,8 +7,8 @@ password: >
 language: de
 role: admin
 history:
+  - tickets
+  - teaser/tickets
   - sponsoren/etrado
   - infos/fuer-einsteiger
-  - teaser/tickets
-  - tickets
   - sponsoren/agile-ruhr
