204b46242630f633968532299c05d187b430cf5f
PLACKPEAUTYII\Maik <wagner@mcwiwa.de>
2017-11-16 09:47:31 +0100

    Piwik auf https, Copyright gesetzt

--- bcruhr11/site/snippets/footer.php
+++ bcruhr11/site/snippets/footer.php
@@ -10,28 +10,30 @@
 <footer id="footer2">
 	<div class="center">
 		<div class="bottom">
-			<p class="copyright">© BarCampRuhr 2016<br>Headerbild <a href="http://steadynews.de">steadynews.de</a></p>
+			<p class="copyright">© BarCampRuhr 2017</p>
 			<p class="copyright"><a href="http://getkirby.com/made-with-kirby-and-love">Made with Kirby and <b>♥</b></a></p>
 			<p class="copyright"><a href="<?php echo url('kontakt/impressum') ?>">Impressum</a></p>
 		</div>
 	</div>
 </footer>
 	<!-- /#footer -->
+
+
 <!-- Piwik -->
 <script type="text/javascript">
   var _paq = _paq || [];
-  _paq.push(["setDomains", ["*.barcamp.ruhr"]]);
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
     var u="//mcwiwa-webservice.de/piwik/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 12]);
+    _paq.push(['setSiteId', '12']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><p><img src="//mcwiwa-webservice.de/piwik/piwik.php?idsite=12" style="border:0;" alt="" /></p></noscript>
+<img src="https://mcwiwa-webservice.de/piwik/piwik.php?idsite=12&rec=1" style="border:0" alt="" />
 <!-- End Piwik Code -->
 </body>
 </html>
