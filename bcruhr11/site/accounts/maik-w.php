4b82836075fde849c8f25df65aa176b7e3511a2d
PLACKPEAUTYII\Maik <wagner@mcwiwa.de>
2018-01-23 12:30:13 +0100

    Ticketdaten aktualisiert

--- bcruhr11/site/accounts/maik-w.php
+++ bcruhr11/site/accounts/maik-w.php
@@ -7,8 +7,8 @@ password: >
 language: de
 role: admin
 history:
+  - tickets
+  - teaser/tickets
   - blog/frohes-neues-und-noch-mehr-tickets
   - sponsoren/lvq
-  - tickets
   - blog/tickets-sponsoren-weihnachten
-  - sponsoren/sipgate
