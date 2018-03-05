bcd3e1545199f010bea324c4bcc645f4fc785a75
PLACKPEAUTYII\Maik <wagner@mcwiwa.de>
2018-03-05 13:06:19 +0100

    Noch ein Tickettermin 07.03.

--- bcruhr11/site/accounts/maik-w.php
+++ bcruhr11/site/accounts/maik-w.php
@@ -7,8 +7,8 @@ password: >
 language: de
 role: admin
 history:
-  - sponsoren/vorwerk
-  - sponsoren/expositio
   - teaser/tickets
   - tickets
+  - sponsoren/vorwerk
+  - sponsoren/expositio
   - kontakt
